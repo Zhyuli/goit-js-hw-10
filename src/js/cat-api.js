8aa2bf4aab248848dc2b8d13922468244c3b761e
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-12-06 10:51:15 +0200

    cat API

---
+++ src/js/cat-api.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_uaCRPCyleObko7mKeYNtnotx414KKcztpuAFo2JTHDyEln3FAuoHBeIQqpgEgUYX";
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
